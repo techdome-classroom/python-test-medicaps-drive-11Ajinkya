2b75b3b83fa0eb4289854b994f01e279053111ae
11Ajinkya <ajinkya.dtarade@gmail.com>
2024-04-25 12:34:38 +0530

    first commit

--- program2.py
+++ program2.py
@@ -4,4 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_to_int_mapping = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            value = roman_to_int_mapping[char]
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+            prev_value = value
+
+        return result
